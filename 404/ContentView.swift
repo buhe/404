0c09e4629f6f51e25f5153fb56d9bfe5a33d0bd6
晒晕的兔子 <gubu1986@126.com>
2022-11-27 15:39:43 +0800

    ui

--- 404/ContentView.swift
+++ 404/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var rawText: String = "A text field will occupy at least the number of lines you specified and then grow as the content grow.A text field will occupy at least the number of lines you specified and then grow as the content grow.A text field will occupy at least the number of lines you specified and then grow as the content grow.A text field will occupy at least the number of lines you specified and then grow as the content grow.A text field will occupy at least the number of lines you specified and then grow as the content grow.A text field will occupy at least the number of lines you specified and then grow as the content grow"
+    @State var encoding: String = "A text field will occupy at least the number of lines you specified and then grow as the content grow.A text field will occupy at least the number of lines you specified and then grow as the content grow.A text field will occupy at least the number of lines you specified and then grow as the content grow.A text field will occupy at least the number of lines you specified and then grow as the content grow.A text field will occupy at least the number of lines you specified and then grow as the content grow.A text field will occupy at least the number of lines you specified and then grow as the content grow"
+    
+    var cities = ["Base64", "Milan", "Venice", "Florence"]
+
+    @State private var selected = "Base64"
+    @State var with: Bool = true
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("rawText", text: $rawText, axis: .vertical).lineLimit(4...20).padding()
+            NavigationView {
+                HStack {
+                 Button{
+                     
+                 } label: {
+                   Text("编码")
+                 }
+                Button{
+                    
+                } label: {
+                  Text("编码")
+                }
+                 Picker("method?", selection: $selected) {
+                     ForEach(cities, id: \.self) {
+                         Text($0)
+                     }
+                 }.frame(minWidth: 100)
+                    Toggle("With", isOn: $with).labelsHidden()
+                        
+                }
+            }.frame(maxHeight: 44)
+   
+            Text(encoding).lineLimit(4...20).padding()
         }
-        .padding()
     }
 }
 
