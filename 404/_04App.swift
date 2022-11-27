94d7c368320314d8f485cc8c6f9805e2ed0be630
晒晕的兔子 <gubu1986@126.com>
2022-11-27 14:13:11 +0800

    Initial Commit

---
+++ 404/_04App.swift
@@ -0,0 +1,17 @@
+//
+//  _04App.swift
+//  404
+//
+//  Created by 顾艳华 on 2022/11/27.
+//
+
+import SwiftUI
+
+@main
+struct _04App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
