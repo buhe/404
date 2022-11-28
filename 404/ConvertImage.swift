cfa5960441c293c861ce0d7e9d63d4007c205a8f
晒晕的兔子 <gubu1986@126.com>
2022-11-28 17:15:27 +0800

    refactor

---
+++ 404/ConvertImage.swift
@@ -0,0 +1,20 @@
+//
+//  ConvertImage.swift
+//  404
+//
+//  Created by 顾艳华 on 2022/11/28.
+//
+
+import SwiftUI
+
+struct ConvertImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConvertImage_Previews: PreviewProvider {
+    static var previews: some View {
+        ConvertImage()
+    }
+}
---
+++ 404/ConvertText.swift
@@ -0,0 +1,20 @@
+//
+//  ConvertText.swift
+//  404
+//
+//  Created by 顾艳华 on 2022/11/28.
+//
+
+import SwiftUI
+
+struct ConvertText: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConvertText_Previews: PreviewProvider {
+    static var previews: some View {
+        ConvertText()
+    }
+}
