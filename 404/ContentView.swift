236a19c718ca3bf5946548958513fe5e698dcc52
晒晕的兔子 <gubu1986@126.com>
2022-11-28 11:56:35 +0800

    add share button

--- 404/ContentView.swift
+++ 404/ContentView.swift
@@ -18,31 +18,35 @@ struct ContentView: View {
     @State var with: Bool = true
 
     var body: some View {
-        VStack {
-            TextField("rawText", text: $vm.model.rawText, axis: .vertical).lineLimit(4...20).padding()
-            NavigationView {
+        ScrollView {
+            VStack {
+                TextField("rawText", text: $vm.model.rawText, axis: .vertical).lineLimit(15...20).padding()
+                NavigationView {
                 HStack {
-                 Button{
-                     vm.encoding()
-                 } label: {
-                   Text("编码")
-                 }
-                Button{
-                    vm.decoding()
-                } label: {
-                  Text("解码")
-                }
-                    Picker("method?", selection: $vm.model.method) {
-                     ForEach(vm.model.methods, id: \.self) {
-                         Text($0)
-                     }
-                 }.frame(minWidth: 100)
-//                    Toggle("With", isOn: $with).labelsHidden()
-                        
-                }
-            }.frame(maxHeight: 44)
-   
-            TextField("resultText", text: $vm.model.encoding, axis: .vertical).lineLimit(4...20).padding()
+                         Button{
+                             vm.encoding()
+                         } label: {
+                           Text("编码")
+                         }
+                        Button{
+                            vm.decoding()
+                        } label: {
+                          Text("解码")
+                        }
+                        Picker("method?", selection: $vm.model.method) {
+                             ForEach(vm.model.methods, id: \.self) {
+                                 Text($0)
+                             }
+                         }.frame(minWidth: 100)
+    //                    Toggle("With", isOn: $with).labelsHidden()
+                        ShareLink(item: vm.model.encoding) {
+                            Label("分享", systemImage:  "square.and.arrow.up")
+                        }
+                    }
+                }.frame(maxHeight: 44)
+       
+                TextField("resultText", text: $vm.model.encoding, axis: .vertical).lineLimit(15...20).padding()
+            }
         }
     }
 }
